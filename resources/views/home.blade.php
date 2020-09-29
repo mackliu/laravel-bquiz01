2e017e79b716aef1d047514f75366c76dbd7b378
mackliu <macklun@gmail.comm>
2020-09-29 22:08:38 +0800

    路由基礎與blade切版

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.layout")
+
+@section("main")
+<div class="menu col-3">選單</div>
+<div class="main col-6">主內容區</div>
+<div class="right col-3">校園映像</div>
+
+
+@endsection
+
+@section("script")
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>科技大學校園資訊系統</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+<div class="container">
+<div class="header w-100">
+    <img src="{{ asset('img/01B01.jpg') }}" alt="" class="w-100">
+</div>
+<div class="main d-flex" style="height:568px">
+    @yield("main")
+</div>
+<div class="footer w-100">
+    <div class="text-center" style="height:100px;line-height:100px;background:yellow">頁尾版權</div>
+</div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
+@yield("script")
