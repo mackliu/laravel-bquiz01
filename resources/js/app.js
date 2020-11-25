5c3df6af4fa5b921fd7dbc2d5c59a16e7d2c4bef
mackliu <macklun@gmail.comm>
2020-11-26 07:09:36 +0800

    vue08 vue開發環境建置

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,94 @@
 require('./bootstrap');
+import { createApp } from 'vue';
+
+const app={
+    data(){
+        return {
+           menus:null,
+           images:null,
+           mvims:null,
+           newss:null ,
+            site:null,
+            show:false
+        }
+    },
+    methods:{
+        switchImg(type){
+          let  imgs=this.images.data;
+          let  page=this.images.page;
+
+            switch(type){
+                case 'up':
+                    page=(page>0)?page-1:page;
+                break;
+                case 'down':
+                    page=(page<imgs.length-3)?++page:page;
+                break;
+            }
+
+            imgs.map((img,idx)=>{
+                if(idx>=page && idx<=page+2){
+                    img.show=true;
+                }else{
+                    img.show=false;
+                }
+                return img;
+            })
+
+            this.images.data=imgs
+            this.images.page=page
+
+        }
+    },
+
+    mounted(){
+        axios.get("/api")
+    .then((res)=>{
+       // console.log(res.data)
+        this.site=res.data.site;
+        this.menus=res.data.menus;
+        this.images=res.data.images;
+        this.newss=res.data.news;
+        this.mvims=res.data.mvims;
+        this.show=true;
+    })
+    
+        let m=1;
+        setInterval(() => {
+            
+            this.mvims.map((mv,idx)=>{
+                mv.show=(idx==m)?true:false
+                return mv
+            })
+        
+            m=(m+1)%this.mvims.length
+
+        }, 3000);
+    }
+}
+
+createApp(app).component('marquee',{
+    template:`
+        <div style="position:relative;width:100%;height:35px;overflow:hidden" ref="marquee">
+            <div style="position:absolute;width:max-content" ref="content">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+    mounted(){
+        let marquee=this.$refs.marquee.offsetWidth;
+        let content=this.$refs.content.offsetWidth;
+        this.$refs.content.style.right=(0-content)+"px";
+        let pos=0-content;
+        setInterval(() => {
+            pos++;
+            this.$refs.content.style.right=pos+"px";
+            if(pos>=marquee){
+                this.$refs.content.style.right=(0-content)+"px";
+                pos=0-content
+            }
+        }, 15);
+
+    }
+}).mount('#app')
+
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>科技大學校園資訊系統</title>
-    <script src="https://unpkg.com/vue@next"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.0/axios.min.js" integrity="sha512-DZqqY3PiOvTP9HkjIWgjO6ouCbq+dxqWoJZ/Q+zPYNHmlnI2dQnbJ5bxAHpAMw+LXRm4D72EIRXzvcHQtE8/VQ==" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container"  id="app">
@@ -28,6 +26,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="./js/app.js"></script>
 </body>
 </html>
 @yield("script")
